602bcbacef01e827d51ff017e21d93cc8afee67c
Anna <annakupenko@googlemail.com>
2025-04-14 13:09:50 +0200

    README _Anna

---
+++ src/main/java/fr/uga/miashs/dciss/chatservice/client/ICQ_versDCISS.java
@@ -0,0 +1,43 @@
+package fr.uga.miashs.dciss.chatservice.client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ICQ_versDCISS {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ICQ_versDCISS window = new ICQ_versDCISS();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ICQ_versDCISS() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
